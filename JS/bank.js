b2070c8c3d3234909e580340a0240c3ea1d171ec
Istiak Iqbal <istiakiqbal@gmail.com>
2023-12-08 22:23:33 +0600

    deposit updated

--- JS/bank.js
+++ JS/bank.js
@@ -1,10 +1,23 @@
 document.getElementById('depositButton').addEventListener('click', function(){
+    // Step 1: Click the button and take input by event handler
     const depositField = document.getElementById('depositInput');
-    const depositInput = depositField.value;
-    
+    const depositInputString = depositField.value;
+    const depositInput = parseFloat(depositInputString) //converted the input element in number
+
+    // Step 2: Get the innertext from html deposit
     const depositOutputField = document.getElementById('depositOutput');
-    const depositOutput = depositOutputField.innerText;
+    const depositOutputString = depositOutputField.innerText;
+    const depositOutput = parseFloat(depositOutputString); //converted the innertext element in number
     
-    depositOutputField.innerText = depositInput
+    //add the values and update on html
+    const currentDeposit = depositInput + depositOutput
+    depositOutputField.innerText = currentDeposit
     depositField.value = ''
+
+    //Total Calculation
+    const totalOutputField= document.getElementById('totalOutput')
+    const totalOutputString = totalOutputField.innerText;
+    const totalOutput = parseFloat(totalOutputString)
+
+    totalOutputField.innerText = totalOutput + currentDeposit;
 })
